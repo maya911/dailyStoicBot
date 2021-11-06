36733fd6b100e7b7f21e9288cf448314474dec7b
pranav911 <>
2021-11-06 15:17:05 +0000

    Added code to grab image and send it as a message, instead of text message.

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import PyPDF2
 import random
 import os
 import telebot
 
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
-quote = "Protect your own good in all that you do, and as concerns everything else take what is given as far as you can make reasoned use of it. If you don’t, you’ll be unlucky, prone to failure, hindered and stymied.     -Epictetus"
-#number = int(random.uniform(0, 5))
-pdfObj = open('quotes.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfObj)
-#pageObj = pdfReader.getPage(number)
 
 @bot.message_handler(commands=['Hi', 'hi', 'hello', 'hey', 'hola'])
 def greet(message):
-    bot.send_message(message.chat.id,quote)
-bot.polling()
+    number = str(int(random.uniform(-1, 4)))
+    print(number)
+    path = "pics/"
+    img = open(path + number + ".png", 'rb')
+    bot.send_photo(message.chat.id, img)
 
+bot.polling()
